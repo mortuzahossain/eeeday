ddfbb06f540419840047425c982efb44e6c1e910
mortuzahossain <mortuzahossain1997@gmail.com>
2017-12-14 12:14:02 +0600

    Done Menu Responsive

---
+++ inc/footer.php
@@ -0,0 +1,140 @@
+
+
+
+    <div class="social-link text-center">
+        <div class="container">
+            <div class="row">
+                <div class="w3-xlarge w3-padding-32">
+                    <a href="#" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"> &nbsp; </i></a>
+                    <a href="#" target="_blank"><i class="fa fa-globe w3-hover-opacity"> &nbsp; </i></a>
+                    <a href="#" target="_blank"><i class="fa fa-map-marker w3-hover-opacity"> &nbsp; </i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="developer text-center">
+        <p>Developer <a href="http://www.facebook.com/mdmortuza.hossain/">Md. Mortuza Hossain</a></p>
+    </div>
+
+    <div class="copyright text-center">
+        <p>All &copy; Received by EEE BAUST </p>
+    </div>
+    
+
+
+
+<div class="modal fade" tabindex="-1" role="dialog" id="ourevents">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Our Events</h4>
+      </div>
+      <div class="modal-body">
+
+        <div class="single-event">
+            <img src="img/event/Techhunt2017_Gaming_Contest_COD4 cover.png" alt="">
+            <div class="right">
+                <h3>Programming Contest</h3>
+                <a class="btn btn-success" href="participate.php">Register</a>
+                <a class="btn btn-success" target="_blank" href="file/event/techhunt_programming_2017_v5.pdf">Event Details</a>
+            </div>
+        </div>
+        
+        <div class="single-event">
+            <img src="img/event/Techhunt2017_Gaming_Contest_COD4 cover.png" alt="">
+            <div class="right">
+                <h3>Programming Contest</h3>
+                <a class="btn btn-success" href="participate.php">Register</a>
+                <a class="btn btn-success" target="_blank" href="file/event/techhunt_programming_2017_v5.pdf">Event Details</a>
+            </div>
+        </div>
+        
+
+     </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div>
+
+
+
+
+<div class="modal fade" tabindex="-1" role="dialog" id="contact-us">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Contact With Us</h4>
+      </div>
+      <div class="modal-body">
+    <p><marquee behavior="alternate" onmouseover="this.stop();" onmouseout="this.start();">Need to change your team info then <a href="#">mail me</a>.</marquee></p>
+        <div class="single-contact text-center">
+            <img src="img/contact/AlMehdiSaadatChowdhury2.jpg" alt="">
+            <div class="contact-detail">
+                <h3>Name</h3>
+                <p>Designation</p>
+                <p>Phone</p>
+                <p>Email</p>
+            </div>
+        </div>
+        
+        <div class="single-contact text-center">
+            <img src="img/contact/AlMehdiSaadatChowdhury2.jpg" alt="">
+            <div class="contact-detail">
+                <h3>Name</h3>
+                <p>Designation</p>
+                <p>Phone</p>
+                <p>Email</p>
+            </div>
+        </div>
+        
+        <div class="single-contact text-center">
+            <img src="img/contact/AlMehdiSaadatChowdhury2.jpg" alt="">
+            <div class="contact-detail">
+                <h3>Name</h3>
+                <p>Designation</p>
+                <p>Phone</p>
+                <p>Email</p>
+            </div>
+        </div>
+        
+
+     </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div>
+
+
+    
+
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.fittext.js"></script>
+    <script src="js/scripts.js"></script>
+
+    <!-- jquery countdown -->
+    <script type="text/javascript" src="js/jquery.plugin.js"></script> 
+    <script type="text/javascript" src="js/jquery.countdown.js"></script>
+    <script type="text/javascript">
+      
+      $(document).ready(function(){
+
+
+        $("#counter").countdown({
+        until: new Date(2018, 1 - 1, 23),
+        format: 'dHMS'
+        });
+
+        $("#counter_wrapper").fitText(1.2, {
+        minFontSize: '20px',
+        maxFontSize: '50px'
+        });
+
+      });
+    </script>
+
+
+</body>
+<html>
---
+++ inc/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/menu.css">
+    <link href="css/custom.css" rel="stylesheet">
+    <title>EEE Day</title>
+</head>
+<body>
+    <div id='cssmenu'>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a data-toggle="modal" data-target="#ourevents">Events</a></li>
+            <li><a target="_blank" href="file/schedule/techhunt_2017_schedule_v1.pdf">Schedule</a></li>
+            <li><a href="teams.php">Teams</a></li>
+            <li><a target="_blank" href="file/schedule/techhunt_2017_schedule_v1.pdf">Results</a></li>
+            <li><a href="sponsors.php">Sponsors</a></li>
+            <li><a data-toggle="modal" data-target="#contact-us" href="#">Contact</a></li>
+        </ul>
+    </div>
+
+    <div class="poster">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center title-logo">
+                    <img class="poster-image" src="img/eee_day.png" alt="Title Logo">
+                </div>
+            </div>
+        </div>
+    </div>
+
