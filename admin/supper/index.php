2f26e7a27be9401156ccc90549f875726e69347d
Mortuza Hossain <mortuzahossain1997@gmail.com>
2017-12-24 19:08:10 +0600

    Start Working With Admin

---
+++ admin/supper/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>EEE Day Supper Admin Entry</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="../css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+    <link href="../css/style.css" rel='stylesheet' type='text/css' />
+    <link href="../css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="login">
+        <h1>Supper Admin Entry Point</h1>
+        <div class="login-bottom">
+            <h2>Login</h2>
+            <form>
+                <div class="col-md-6">
+                    <div class="login-mail">
+                        <input type="text" placeholder="Email" required="">
+                        <i class="fa fa-envelope"></i>
+                    </div>
+                    <div class="login-mail">
+                        <input type="password" placeholder="Password" required="">
+                        <i class="fa fa-lock"></i>
+                    </div>
+                </div>
+                <div class="col-md-6 login-do">
+                    <label class="hvr-shutter-in-horizontal login-sub">
+					<input type="submit" value="login">
+                </div>
+
+                <div class="clearfix"> </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
