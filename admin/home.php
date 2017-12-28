3d4e66f91f5c48d713fb588bdbc7ce513948271c
Mortuza Hossain <mortuzahossain1997@gmail.com>
2017-12-29 00:09:01 +0600

    Done the Basic Part Of the Index Page of admin

---
+++ admin/home.php
@@ -0,0 +1,50 @@
+<?php
+    include('inc/top.php');
+?>
+<h1>Home Page Content</h1>
+<hr>
+
+<?php
+/*
+	if (isset($_POST['save'])) {
+		$copyright_text = addslashes($_POST['copyright_text']);
+
+		$updatequery = "UPDATE contents SET content='$copyright_text' WHERE slug ='copyright_text'";
+		if (mysqli_query($con,$updatequery)) {
+			echo "Data Update Sucessfully";
+		} else {
+			echo "Error Please Try Again";
+		}
+		
+	}
+	// For show the previous data
+	if ($con) {
+		$sql = "SELECT content FROM contents WHERE slug ='copyright_text'";
+		$result = mysqli_query($con,$sql)->fetch_assoc();
+		//var_dump($result);
+	}
+	*/
+
+?>
+
+
+	<div class="grid-form1">
+	<h3 id="forms-horizontal"># Link Of The Poster Image</h3>
+	<form class="form-horizontal" action="" method="post">
+	  <div class="form-group">
+	    <label for="footertext" class="col-sm-2 control-label hor-form">Copyritht Text</label>
+	    <div class="col-sm-10">
+	      <input type="text" name="copyright_text" class="form-control" id="footertext" value="<?php echo $result['content']; ?>">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+	      <button type="submit" class="btn btn-default" name="save">Save</button>
+	    </div>
+	  </div>
+	</form>
+	</div>
+<hr>
+<?php
+    include('inc/bottom.php');
+?>
--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,102 @@
     include('inc/top.php');
 ?>
 
+<div class="col-md-4 ">
+	<div class="content-top-1">
+	<div class="col-md-6 top-content">
+		<h5>Approved</h5>
+		<label>8761</label>
+	</div>
+	<div class="col-md-6 top-content1">	   
+		<div id="demo-pie-1" class="pie-title-center" data-percent="25"> <span class="pie-value"></span> </div>
+	</div>
+	 <div class="clearfix"> </div>
+	</div>
+	<div class="content-top-1">
+	<div class="col-md-6 top-content">
+		<h5>Pending</h5>
+		<label>6295</label>
+	</div>
+	<div class="col-md-6 top-content1">	   
+		<div id="demo-pie-2" class="pie-title-center" data-percent="50"> <span class="pie-value"></span> </div>
+	</div>
+	 <div class="clearfix"> </div>
+	</div>
+	<div class="content-top-1">
+	<div class="col-md-6 top-content">
+		<h5>Rejected</h5>
+		<label>3401</label>
+	</div>
+	<div class="col-md-6 top-content1">	   
+		<div id="demo-pie-3" class="pie-title-center" data-percent="75"> <span class="pie-value"></span> </div>
+	</div>
+	 <div class="clearfix"> </div>
+	</div>
+</div>
+<div class="col-md-8 content-top-2 ">
+	<h1 class="padding20">All Participance</h1>
+	<select class="form-control ">
+		<option>All</option>	
+		<option>Matlab</option>
+	</select>
 
+<?php
+    $sql = "SELECT * FROM participance2018 ORDER BY id DESC" ;
+    $result = mysqli_query($con,$sql);
+    $have_data = mysqli_num_rows($result);
+
+    if ($have_data) {
+		while($row = mysqli_fetch_assoc($result)){
+            $participance[] = $row;
+        }
+        //var_dump($participance);
+        $serial_no =0;
+
+?>
+
+	<table class="table table-striped ">
+        <tr>
+            <td width="5%">SL</td>
+            <td width="35%%">Name</td>
+            <td width="40%">Institution Name</td>
+            <td width="10%">Status</td>
+            <td width="10%">Details</td>
+        </tr>       
+<?php foreach ($participance as $key) { $serial_no++; ?>
+        <tr>
+            <td width="5%"><?php echo $serial_no; ?></td>
+            <?php if(!empty($key['name'])) {?>
+            <td width="35%"><?php echo $key['name']; ?></td>
+            <?php } else { ?>
+            <td width="35%"><?php echo $key['teamname']; ?></td>
+            <?php } ?>
+            <td width="40%"><?php echo $key['institute']; ?></td>
+            <?php if($key['status'] == '0'){ ?>
+            <td width="10%" class="pending text-center">
+                <p>Pending</p>
+            </td>
+            <?php } elseif ($key['status'] == '1') { ?>
+            <td width="10%" class="accepted text-center">
+                <p>Accepted</p>
+            </td>
+            <?php } else { ?>
+            <td width="10%" class="rejected text-center">
+                <p>Rejected</p>
+            </td>
+            <?php } ?>
+            <td width="10%"><a href="details.php?id=<?php echo $key['id']; ?>" target="_blank" class="btn btn-success mybun">Details</a></td>
+        </tr>       
+<?php } ?>
+	</table>
+
+<?php } else {
+	echo "No Participance Registered Yet.";
+}
+?>
+
+</div>
+
+<div class="clearfix"> </div>
 <?php
     include('inc/bottom.php');
 ?>
